de68bf0f106c720b96e6cd2677a1569c729726f9
Eric C Schoenborn <cultureondemand@gmail.com>
2018-12-14 12:58:58 -0500

    Add files via upload

---
+++ assets/js/content.js
@@ -0,0 +1,167 @@
+var slideData = [
+
+  {
+    section: "We turned real life into a .GIF and skated it.",
+//////////////////     icon: "pcy.svg",
+    header: "",
+ //////////////////////////   video: "yes",
+    bkgImage: {
+      small: "phenomenalradnessartpark_s.jpg",
+      medium: "phenomenalradnessartpark.jpg",
+      large: "phenomenalradnessartpark.jpg"
+      
+    },
+    body: `
+Phenomenal Radness was orginially created in partnership with NikeSB and MIA Skateshop as a popup  experience in the Miami Design District. 
+
+    `
+  },
+  
+
+
+  {
+    section: "Social Augmented Reality.",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    bkgImage: {
+      small: "mirrortravel_pr.png",
+      medium: "mirrortravel_pr.png",
+      large: "mirrortravel_pr.png"
+    },
+    body: `
+    The augmented reality environment provides a unique three-dimensional sound and motion responsive experience, using only common Web and projector technologies. 
+     `
+  },
+  
+
+
+
+
+
+  {
+    section: "Art Robots that can hear and see.",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    video: "yes",
+   bkgImage: {
+     wm: "assets/images/bkg/lilwayne.webm",
+       mp: "assets/images/bkg/lilwayne.mp4",
+      name:"1"
+    },
+    body: `
+ Phenomneal Radness is constantly evolving, as we adapt and customize the brains of the piece (JavaScript) and the projection mapping approach for new contexts.
+   
+    `
+  },
+  
+
+
+
+  {
+    section: "Instant generative art installation.",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    video: "yes",
+   bkgImage: {
+     wm: "assets/images/bkg/prmusic.webm",
+       mp: "assets/images/bkg/prmusic.mp4",
+      name:"2"
+    },
+    body: `
+We have generated Phenomenal Radness for galleries, museums, events, festivals, musicians, venues, skateparks and more.  
+    
+    
+    `
+  },
+  
+
+
+
+
+  {
+    section: "One projector and a laptop.",
+header: "",
+    bkgImage: {
+      small: "projector2_s.gif",
+      medium: "projector2.gif",
+      large: "projector2.gif"
+    },
+
+    body:`
+ Full scale architecture projection mapping installations or a simple augmented reality environments, from a Web browser.
+ `
+
+  }
+
+,
+  
+
+
+
+
+  {
+    section: "Created by Eric Cade Schoenborn and Ed Selego.",
+header: "",
+    bkgImage: {
+      small: "20160408_inq_hd1skate08z-a.JPG",
+      medium: "20160408_inq_hd1skate08z-a.JPG",
+      large: "20160408_inq_hd1skate08z-a.JPG"
+    },
+
+    body:`
+ We are 100% about creating innovative experiences through collaboration and fun.
+
+ `
+
+  }
+
+,
+
+ 
+ 
+
+
+  {
+    section: "Phenomenal Radness is indestructible.",
+//////////////////////////     icon: "pcy.svg",
+    header: "",
+    video: "yes",
+   bkgImage: {
+     wm: "assets/images/bkg/prwr.webm",
+       mp: "assets/images/bkg/prwr.mp4",
+      name:"3"
+    },
+    body: `
+    It has been an absolute pleasure to create each custom expereince, and see how people make it their own. 
+    `
+  },
+  
+
+
+
+
+  {
+    section: "Thanks for coming through.",
+header: "",
+    bkgImage: {
+      small: "prcrowd2.gif",
+      medium: "prcrowd2.gif",
+      large: "prcrowd2.gif"
+    },
+
+    body:`
+We would love to beam this for you. Reach out if you are interested in knowing more.
+<br />
+<br /> <a href="mailto:&#99;&#117;&#108;&#116;&#117;&#114;&#101;&#111;&#110;&#100;&#101;&#109;&#97;&#110;&#100;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;">+ Drop us a line</a>
+<br />
+
+
+ `
+
+  }
+
+ 
+
+
+
+];
