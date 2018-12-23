90edb14287c44dc497c21d03d56e23b676cec768
Eric C Schoenborn <cultureondemand@gmail.com>
2018-12-23 00:25:15 -0500

    Update content.js

--- assets/js/content.js
+++ assets/js/content.js
@@ -12,7 +12,7 @@ var slideData = [
       name:"1"
     },
     body: `
-<strong>Sound-reactive augmented reality projection mapping installation for Catharsis on the National Mall 2018.</strong> Developed Javascript browser app in Three.js to drive installation created with 3 projectors hung from the frame of a 30'x30' dome constructed in the center of the National Mall. Mapped the app using MadMapper.  
+<strong>Catharsis on the National Mall 2018</strong><br />Video, JavaScript, WebGL, Projection Mapping<br />124' x 42'<br />October 5, 2018<br /><br /> Developed Javascript browser app in Three.js to drive installation created with 3 projectors hung from the frame of a 30'x30' dome constructed in the center of the National Mall. Mapped the app using MadMapper.  
     `
   },
   
@@ -31,7 +31,7 @@ var slideData = [
       
     },
     body: `
-<strong>Guerilla Kavanaugh Projections</strong><br />Video, JavaScript, WebGL, Projection Mapping<br />24' x 42'<br />October 5, 2018<br />Installed for Eaton House Human Progress Festival<br /><br /> Outdoor projection mapping program on landmarks along the power center of DC during the Brett Kavanaugh confirmation hearings. Created content 
+<strong>Guerilla Kavanaugh Projections</strong><br />Video, JavaScript, WebGL, Projection Mapping<br />24' x 42'<br />October 5, 2018<br /><br /> Outdoor projection mapping program on landmarks along the power center of DC during the Brett Kavanaugh confirmation hearings. Created content 
 
     `
   },
